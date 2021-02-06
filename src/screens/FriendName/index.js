6d1191df113893effb45a5b3daf6813280fdaaa9
prmstrgt <pramestiregitaa@gmail.com>
2021-02-06 17:27:00 +0700

    show Friend Name

--- src/screens/FriendName/index.js
+++ src/screens/FriendName/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {useNavigation} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import IconMa from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import styled from './style';
 
 export default function FriendName() {
+  const {friend} = useSelector((state) => state);
+
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  const goHome = () => {
+    dispatch({type: 'CLEAR'});
+    navigation.navigate('Home');
+  };
+
   return (
-    <View>
-      <Text>Friend</Text>
+    <View style={styled.parent}>
+      <View style={styled.title}>
+        <Icon name="check-circle" size={60} color="green" />
+        <Text style={styled.titleText}>Success</Text>
+      </View>
+      <View style={styled.content}>
+        <Text style={styled.subTitle}>Your beloved friend name is</Text>
+        <Text style={styled.name}>{friend}</Text>
+      </View>
+      <TouchableOpacity onPress={() => goHome()}>
+        <IconMa name="home" size={50} />
+      </TouchableOpacity>
     </View>
   );
 }
---
+++ src/screens/FriendName/style.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  parent: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    marginTop: 50,
+    marginBottom: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  subTitle: {
+    fontSize: 20,
+  },
+  name: {
+    fontSize: 45,
+    fontWeight: 'bold',
+    color: '#588bae',
+  },
+});
